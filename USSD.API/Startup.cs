86280cc80cfd8ee20ea09f5917ac9da8d6cb63ba
Nodirbek-Abdulaxadov <nbkabdulaxadov@gmail.com>
2022-02-10 23:06:09 +0500

    dasc

--- USSD.API/Startup.cs
+++ USSD.API/Startup.cs
@@ -39,10 +39,10 @@ namespace USSD.API
                 options.UseMySql(Configuration.GetConnectionString("MySqlDB"), MySqlServerVersion.Parse("5.7.37")), ServiceLifetime.Scoped);
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "USSD.API", Version = "v1" });
-            });
+            //services.AddSwaggerGen(c =>
+            //{
+            //    c.SwaggerDoc("v1", new OpenApiInfo { Title = "USSD.API", Version = "v1" });
+            //});
         }   
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,10 +51,10 @@ namespace USSD.API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();                
-            }
 
-            app.UseSwagger();
-            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "USSD.API v1"));
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "USSD.API v1"));
+            }
 
             app.UseHttpsRedirection();
 
