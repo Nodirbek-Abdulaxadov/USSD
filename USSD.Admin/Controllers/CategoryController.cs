cfa55f2b77d06a78a798b681a90db8cb3505d833
davronoff <ilyosbekdavronov009@gmail.com>
2022-03-23 14:25:46 +0300

    some

---
+++ USSD.Admin/Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace USSD.Admin.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
