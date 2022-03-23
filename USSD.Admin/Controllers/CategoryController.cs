a9338b4d0c1a3e1b89ba183f24f3a4bc12d65e29
davronoff <ilyosbekdavronov009@gmail.com>
2022-03-23 16:29:33 +0300

    add view page

--- USSD.Admin/Controllers/CategoryController.cs
+++ USSD.Admin/Controllers/CategoryController.cs
@@ -1,12 +1,52 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+using USSD.Data.Models;
+using USSD.Data.Services;
 
 namespace USSD.Admin.Controllers
 {
     public class CategoryController : Controller
     {
-        public IActionResult Index()
+        private readonly ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+        public async  Task<IActionResult> Index()
+        {
+            var item = await _categoryService.GetCategories();
+            return View(item);
+        }
+        [HttpGet]
+        public IActionResult Add()
         {
             return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Add(Category category)
+        {
+            await _categoryService.AddCategory(category);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var item = await _categoryService.GetCategory(id);
+            return View(item);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(Category category)
+        {
+            var item = await _categoryService.UpdateCategory(category);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            _categoryService.DeleteCategory(id);
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ USSD.Admin/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using USSD.Data.Services;
+
+namespace USSD.Admin.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService) 
+        {
+            _productService = productService;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var item = await _productService.GetProducts();
+            return View(item);
+        }
+    }
+}
